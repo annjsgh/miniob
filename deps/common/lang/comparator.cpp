8176f24156114aac9c40365d0c6963c9ea2e0abb
annjsgh <2483779035@qq.com>
2024-05-28 21:40:14 +0800

    two

--- deps/common/lang/comparator.cpp
+++ deps/common/lang/comparator.cpp
@@ -65,11 +65,15 @@ int compare_date(void* arg1, void* arg2){
 }
 
 int compare_str_with_int(void *arg1, int arg1_max_length, void *arg2) {
-  
+  int temp2 = *(int *)arg2;
+  const char* str2 = (std::to_string(temp2)).c_str();
+  return compare_string(arg1, arg1_max_length, (void*)str2, (std::to_string(temp2)).size());
 }
 
 int compare_str_with_float(void *arg1, int arg1_max_length, void *arg2) {
-
+  float temp2 = *(float *)arg2;
+  const char* str2 = (std::to_string(temp2)).c_str();
+  return compare_string(arg1, arg1_max_length, (void*)str2, std::to_string(temp2).size());
 }
 
 } // namespace common
